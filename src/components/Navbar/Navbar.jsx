89cd6c8488b4088357134cc8ae6d4d8356b3674c
Alexander Torres <alexander20012@hotmail.com>
2023-08-21 12:52:42 -0400

    cambio de texto en el boton de about

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,17 +5,17 @@ const Navbar = () => {
     return (
         <nav className="container navbar navbar-expand-lg">
             <div className="container-fluid">
-                <a className="navbar-brand" href=""><img className='nav-logo' src={logo} alt="" /></a>
+                <a className="navbar-brand" href="#"><img className='nav-logo' src={logo} alt="" /></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end me-5" id="navbarSupportedContent">
                     <ul className="navbar-nav">
                         <li className="nav-item mx-2">
-                            <a className="nav-link" aria-current="page" href="">Home</a>
+                            <a className="nav-link" aria-current="page" href="/">Home</a>
                         </li>
                         <li className="nav-item mx-2">
-                            <a className="nav-link" href="">Abaut Us</a>
+                            <a className="nav-link" href="/">About Us</a>
                         </li>
                         <li className="nav-item mx-2">
                             <a className="nav-link" href="/login">Login</a>
