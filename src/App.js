070c332a72e0e8b50395e1955fdff33f6b082bc7
Alexander Pedro Jose Torres Apontes <alexander20012@hotmail.com>
2023-08-22 13:32:34 -0400

    cambios

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/App.css';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
-<<<<<<< HEAD
 // import About from './components/About/About';
 import About from './components/Landing_Page/About/About';
-=======
-
->>>>>>> 59f47cdd00040acfd78b2e8657dcf16ccff7d0d7
 
 function App(props) {
   return (
