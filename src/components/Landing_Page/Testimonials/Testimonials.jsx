25c940b3bedcb8a87dfbec980fbfc57a33667c8e
Melissa Lopez <melilo89@gmail.com>
2023-08-22 03:08:21 -0300

    carousel con testimonios

--- src/components/Landing_Page/Testimonials/Testimonials.jsx
+++ src/components/Landing_Page/Testimonials/Testimonials.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Carousel, Card } from 'react-bootstrap';
+import { dataTestimonials } from './dataTestimonials';
+import styles from './Testimonials.module.css'
 
-const Testimonials = () => {
-    return (
-        <>
-            <section className='d-flex align-items-center justify-content-center vh-100 bg-light'>Testimonials</section>
-        </>
-    )
-}
+const TestimonialsCarousel = () => {
+  const itemsPerPage = window.innerWidth <= 768 ? 1 : 3; // Mostrar 1 card en pantalla pequeña, 3 en pantalla grande
+  const totalItems = dataTestimonials.length;
+  const totalGroups = Math.ceil(totalItems / itemsPerPage);
 
-export default Testimonials
+  const [activeGroup, setActiveGroup] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newItemsPerPage = window.innerWidth <= 768 ? 1 : 3;
+      if (newItemsPerPage !== itemsPerPage) {
+        setActiveGroup(0);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [itemsPerPage]);
+
+  const handleSelect = (selectedIndex, e) => {
+    e.preventDefault();
+    setActiveGroup(selectedIndex);
+  };
+
+  const renderTestimonialsGroup = (groupIndex) => {
+    const startIndex = groupIndex * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    return dataTestimonials.slice(startIndex, endIndex).map((testimony, idx) => (
+      <Card key={startIndex + idx} className={styles.card}>
+        <div className={styles.image}>
+          <img src={testimony.image} alt={testimony.image} />
+        </div>
+      
+        <h3 style={{fontSize:'12px', height:'80px'}}>{testimony.testimony}</h3>
+        <h2 style={{height:'30px', fontSize:'20px', fontWeight:'bolder'}}>{testimony.name}</h2>
+      </Card>
+    ));
+  };
+
+  return (
+    <div className={styles.testimonialsContainer}>
+      <h2 style={{color:'black', fontWeight:'bolder'}}>TESTIMONIOS</h2>
+      <br />
+      <Carousel
+        activeIndex={activeGroup}
+        onSelect={handleSelect}
+        interval={null}
+        wrap={true}
+        variant='dark'
+        className={styles.carousel}
+indicators={false}
+      >
+        {Array.from({ length: totalGroups }).map((_, groupIndex) => (
+          <Carousel.Item key={groupIndex}>
+            <div className={styles.carouselRow}>
+              {renderTestimonialsGroup(groupIndex)}
+            </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default TestimonialsCarousel;
