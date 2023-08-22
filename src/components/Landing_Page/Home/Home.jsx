55076a9ee288eeb83670f4a44c26040cb334d875
Melissa Lopez <melilo89@gmail.com>
2023-08-21 23:52:52 -0300

    rama creada - mostrando planes y servicios

--- src/components/Landing_Page/Home/Home.jsx
+++ src/components/Landing_Page/Home/Home.jsx
@@ -5,14 +5,16 @@ import Mision from '../Mision/Mision'
 import Abaut from '../Abaut/Abaut'
 import Testimonials from '../Testimonials/Testimonials'
 import Footer from '../Footer/Footer'
+import Services from '../../Services/Services'
 
 const Home = () => {
-    return (
+    return (   
         <>
             <Navbar />
             <Header />
             <Mision />
             <Abaut />
+            <Services/>
             <Testimonials />
             <Footer />
         </>
--- src/components/Landing_Page/Testimonials/Testimonials.jsx
+++ src/components/Landing_Page/Testimonials/Testimonials.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Testimonials = () => {
     return (
         <>
-            <section className='d-flex align-items-center justify-content-center vh-100 bg-light'>Testimonials</section>
+            <section className='d-flex align-items-center justify-content-center vh-100' style={{background:'#f5f5f5' }}>Testimonials</section>
         </>
     )
 }
