7add4049ada6e4a307f03662a35cb8502dcaab50
MaxyMarquez <maxy.marquez@hotmail.com>
2023-08-24 01:27:39 -0300

    creacion del componente about us y modificacion del nav

--- src/components/Landing_Page/Navbar/Navbar.jsx
+++ src/components/Landing_Page/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="container navbar navbar-expand-lg">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#"><img className='nav-logo' src={logo} alt="" /></a>
+                <a className="navbar-brand" href="/"><img className='nav-logo' src={logo} alt="" /></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -15,7 +15,7 @@ const Navbar = () => {
                             <a className="nav-link" aria-current="page" href="/">Inicio</a>
                         </li>
                         <li className="nav-item mx-2">
-                            <a className="nav-link" href="/">Conocenos</a>
+                            <a className="nav-link" href='/about_us'>Conocenos</a>
                         </li>
                         <li className="nav-item mx-2">
                             <a className="nav-link" href="/login">Iniciar Sesion</a>
