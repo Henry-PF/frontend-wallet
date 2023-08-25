ac081028602656d9329b734303030dab246927ee
Melissa Lopez <melilo89@gmail.com>
2023-08-25 01:40:28 -0300

    simulacion de transferencia

--- src/components/User_Panel/Dashboard/QuickTransaction/QuickTransaction.jsx
+++ src/components/User_Panel/Dashboard/QuickTransaction/QuickTransaction.jsx
@@ -1,57 +1,101 @@
 import React from "react";
-import styles from './QuickTransaction.module.css'
+import styles from "./QuickTransaction.module.css";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateSaldo } from "../../../../redux/actions";
+
 
 const QuickTransaction = () => {
+  const [selectedButton, setSelectedButton] = useState(null);
 
-  const user = {
-    name: "Pepito",
-    id: "222-fff",
-    saldo: 200000,
-    favorites: [
-      { name: "Nombre1", id: "id1" },
-      { name: "Nombre2", id: "id2" },
-      { name: "Nombre3", id: "id3" },
-    ],
+  const [amount, setAmount] = useState("");
+  const [error, setError] = useState("");
+  const {favorites, saldo} = useSelector(state => state.user);
+  console.log(favorites, 'fav de user DE REDUX')
+  console.log(amount, 'soy el amount')
+  const dispatch = useDispatch();
+
+  const handleSelected = (id) => {
+    if (selectedButton === id) {
+      setSelectedButton(null); // Deselecciona si el botón ya está seleccionado
+    } else {
+      setSelectedButton(id); // Selecciona si el botón no está seleccionado
+    }
   };
+  const handleAmountChange = (event) => {
+    const input = event.target.value;
+    const saldoActual = saldo; // Obtén el saldo actual del usuario
+  
+    if (/^[0-9]*$/.test(input) || input === "") {
+      setAmount(input);
+      setError("");
+  
+      if (input !== "" && parseFloat(input) > saldoActual) {
+        setError("Saldo insuficiente");
+      } else {
+        setError(""); // Borra el mensaje de error si el monto es válido
+      }
+    } else {
+      setError("Solo se admiten números");
+    }
+  };
+  
+
+  const handleButtonTransfer = () => {
+    if (!selectedButton || amount === "") {
+      alert("Debe seleccionar usuario y monto a enviar");
+    } else {
+      const saldoActual = saldo;
+      const nuevoSaldo = saldoActual - amount; // Asegúrate de convertir el amount a número
+      
+      dispatch(updateSaldo(nuevoSaldo)); // Dispatch de la acción con el nuevo saldo
+      alert("Transferencia realizada con éxito");
+      setSelectedButton(null);
+      setAmount("")
+    }
+  }
 
   return (
     <div className={styles.QuickTransactionContainer}>
       <div className={styles.text}>
         <h3>Transferencia Rápida</h3>
-        <h3>Dinero disponible ${user.saldo}</h3>
+        <h3>Dinero disponible ${saldo}</h3>
       </div>
 
       <div className={styles.favorites}>
-  {user.favorites.map((e) => (
-    <div key={e.id}>
-      <button
-        onClick={() => {
-          alert("Favorito seleccionado");
-        }}
-        className={styles.buttonFav}
-        id={e.id}
-      >        {e.name.charAt(0)}
-      </button>
-      <p >{e.name}</p>
+      {favorites?.map((e) => (
+        console.log(e, 'cada elemento de favorites'),
+        <div key={e.id}>
+          <button
+            onClick={() => handleSelected(e.id)}
+            className={`${styles.buttonFav} ${selectedButton === e.id ? styles.selected : ''}`}
+          >
+            {e.name.charAt(0)}
+          </button>
+          <p>{e.name}</p>
+        </div>
+      ))}
     </div>
-  ))}
-</div>
-
 
       <div className={styles.transferContainer}>
-        <input
+        <div className={styles.inputError}>
+          <input
           type="text"
           placeholder="Ingresa el monto"
-          className={styles.inputTransfer}
+          className={`${styles.inputTransfer} ${error && styles.errorInput}`}
+          value={amount}
+          onChange={handleAmountChange}
         />
+        <p className={styles.errorMessage}>{error}</p>
+        </div>
+      
         <button
-          className={styles.buttonTransfer}
-          onClick={() => {
-            alert("debe seleccionar usuario y monto a enviar");
-          }}
-        >
-          Enviar
-        </button>
+  className={styles.buttonTransfer}
+  onClick={handleButtonTransfer}
+>
+  Enviar
+</button>
+
       </div>
     </div>
   );
